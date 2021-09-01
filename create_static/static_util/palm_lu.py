8a14c247faf95c4b480277a21c3fd0cb4aa4c2e3
Dongqi-DQ <dorseylin@outlook.com>
2021-09-01 20:16:00 +1200

    add albedo function for urban buildup

--- create_static/static_util/palm_lu.py
+++ create_static/static_util/palm_lu.py
@@ -9,7 +9,8 @@
 # Acknowledgement: Some parts of the land use classification function 
 #                  is based on Ricardo Faria's WRF2PALM tools. 
 #--------------------------------------------------------------------------------#
-
+import pandas as pd
+import numpy as np
 
 def lu2palm(array, classification):
 
@@ -18,9 +19,6 @@ def lu2palm(array, classification):
     The classification can be:
         'vegetation', 'pavement', 'building', 'water', 'soil', etc.
     '''
-    
-    import pandas as pd
-    import numpy as np
     
     tab = pd.read_csv('raw_static/nzlcdb_2_PALM_num.csv')
     array_out = np.zeros_like(array)
@@ -40,14 +38,50 @@ def lu2palm(array, classification):
     elif classification == 'soil':
         for l in range(0,tab.shape[0]):
             array_out[array[:,:] == tab.iloc[l,0]] = tab.iloc[l,5]
-
-
+   
+    # check NaNs for water
     if classification == 'water':
         array_out[np.isnan(array)] = 2
     else:
         array_out[np.isnan(array_out)] = -9999.0
         array_out[np.isnan(array)] = -9999.0
-    
+    # remove zeros
     array_out[array_out[:,:]==0] = -9999.0
     return(array_out)
+
+def get_albedo(array, classification):
+    '''
+    The classification can be:
+        'vegetation', 'pavement', 'building', 'water', 'soil', etc.
+    Note that for all building types, albedo type is 33
+              for all water types, albedo type is 1
+    This may change in future versions of PALM.
+    '''
+    
+    array_out = np.zeros_like(array)
+    
+    if classification == 'vegetation':
+        tab = pd.read_csv('raw_static/vegetation_to_albedo.csv')
+        for l in range(0,tab.shape[0]):
+            array_out[array[:,:] == tab.iloc[l,0]] = tab.iloc[l,1]
+    elif classification == 'pavement':
+        tab = pd.read_csv('raw_static/pavement_to_albedo.csv')
+        for l in range(0,tab.shape[0]):
+            array_out[array[:,:] == tab.iloc[l,0]] = tab.iloc[l,1]
+    elif classification == 'building':
+        array_out[array[:,:] > 0] = 33
+
+    # check NaNs for water
+    if classification == 'water':
+        array_out[array>0] = 1
+    else:
+        array_out[np.isnan(array_out)] = -9999.0
+        array_out[np.isnan(array)] = -9999.0
+    
+    # where no types are given, keep albedo type as 0 for combination
+    array_out[array_out[:,:]<=0] = 0.0
+
+
+    return array_out
+
 
